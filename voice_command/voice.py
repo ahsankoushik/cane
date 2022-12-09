83a8f19204694e4e73498686d529227b5fd6427f
ahsankoushik <md.ahsan.hasib@g.bracu.ac.bd>
2022-12-09 19:04:08 +0600

    merged

--- voice_command/voice.py
+++ voice_command/voice.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import pyttsx3
-import os
-
-engine = pyttsx3.init()
-
-# voices = engine.getProperty('voices')
-
-# for x in voices:
-#     print(x)
-
-# engine.setProperty('voice','bengali')
-# engine.setProperty('bit',)
-
-
-def speak(s:str):
-    engine.say(s)
-    engine.runAndWait()
-
-def save(s,path):
-    engine.save_to_file(s,path)
-    engine.runAndWait()
-    
-
-# speak('hey this is kanak')
-
-if __name__ == '__main__':
-    speak('hello world')
-    save('this is koushik','test.mp3')
-=======
 import os 
 
 
@@ -37,4 +7,3 @@ def speak(string:str):
 
 if __name__ == '__main__':
     speak('hey this is john doe')
->>>>>>> origin/k_main
