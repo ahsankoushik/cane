f21f536694b4abc6003f8430b533bd4da2eb6307
ahsankoushik <md.ahsan.hasib@g.bracu.ac.bd>
2023-02-01 18:10:27 +0600

    ignored

---
+++ test/anoth.py
@@ -0,0 +1,12 @@
+from voice_command import old_vopice
+old_vopice.save('music mode is selected.','music_mode.mp3')
+
+
+# from playsound import playsound
+# from music import player
+
+# player.play_for_once('/home/koushik/github/cane/voice_command/commads/image_detection.mp3')
+
+
+# import os
+# print(type(os.getcwd()))
--- voice_command/test.py
+++ voice_command/test.py
@@ -1,12 +1,12 @@
-import voice
+import voice_command.old_vopice as old_vopice
 
-voice.engine.save_to_file('music mode is selected','music_mode.mp3')
-voice.engine.runAndWait()
-voice.engine.save_to_file('walking mode is selected','walking_mode.mp3')
-voice.engine.runAndWait()
+old_vopice.engine.save_to_file('music mode is selected','music_mode.mp3')
+old_vopice.engine.runAndWait()
+old_vopice.engine.save_to_file('walking mode is selected','walking_mode.mp3')
+old_vopice.engine.runAndWait()
 
-voice.engine.save_to_file('image read mode is selected', 'image_read.mp3')
-voice.engine.runAndWait()
+old_vopice.engine.save_to_file('image read mode is selected', 'image_read.mp3')
+old_vopice.engine.runAndWait()
 
-voice.engine.save_to_file('image detection mode is selected', 'image_detection.mp3')
-voice.engine.runAndWait()
+old_vopice.engine.save_to_file('image detection mode is selected', 'image_detection.mp3')
+old_vopice.engine.runAndWait()
